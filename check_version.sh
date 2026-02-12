9ec8fb83e2b9e94b145c923db78de2a779f1c2d1
Martin Klose <109064+mklose@users.noreply.github.com>
2026-02-12 17:23:07 +0100

    use dotnet 9.x

---
+++ check_version.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -Eeu
+
+readonly REGEX="image_name\": \"(.*)\""
+readonly JSON=`cat docker/image_name.json`
+[[ ${JSON} =~ ${REGEX} ]]
+readonly IMAGE_NAME="${BASH_REMATCH[1]}"
+
+readonly MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly EXPECTED=9.0.311
+readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'dotnet --version')
+
+if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
+  echo "VERSION CONFIRMED as ${EXPECTED}"
+else
+  echo "VERSION EXPECTED: ${EXPECTED}"
+  echo "VERSION   ACTUAL: ${ACTUAL}"
+  exit 42
+fi
