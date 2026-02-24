f50a4acc943d645ec97881cda48af03649295eed
JonJagger <jon@kosli.com>
2026-02-24 13:24:03 +0000

    Move red-amber-green lambda to start-point

--- docker/red_amber_green.rb
+++
@@ -1,7 +0,0 @@
-
-lambda { |stdout,stderr,status|
-  output = stdout + stderr
-  return :green if /^.*Test execution complete\n.*test succeeded/.match(output)
-  return :red   if /^.*Test execution complete\n.*test failed/.match(output)
-  return :amber
-}
